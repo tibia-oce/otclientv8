4172027186d55375f9b13d0010189f57c3c9ea67
Jordan Hoare <jordanhoare0@gmail.com>
2025-01-11 14:53:55 +0800

    findlizip.cmake

--- src/framework/cmake/FindLibZip.cmake
+++ src/framework/cmake/FindLibZip.cmake
@@ -1,39 +1,53 @@
-# FindLibZip.cmake
-include(FindPackageHandleStandardArgs)
+find_package(PkgConfig)
+
+# Try pkg-config first
+pkg_check_modules(PC_LIBZIP QUIET libzip)
+
+find_path(LIBZIP_INCLUDE_DIR
+    NAMES zip.h
+    PATHS
+    ${PC_LIBZIP_INCLUDE_DIRS}
+    ${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include
+    PATH_SUFFIXES libzip
+)
 
 if(WIN32)
-    find_path(LIBZIP_INCLUDE_DIR_ZIP
-        NAMES zip.h
-        PATHS ${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include
-        PATH_SUFFIXES libzip
-    )
-    
-    find_path(LIBZIP_INCLUDE_DIR_ZIPCONF
-        NAMES zipconf.h
-        PATHS ${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include
-    )
-    
+    # On Windows, try both .lib and .dll
     find_library(LIBZIP_LIBRARY
-        NAMES zip libzip
-        PATHS ${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib
+        NAMES 
+            zip libzip
+            zip.lib libzip.lib
+        PATHS
+            ${PC_LIBZIP_LIBRARY_DIRS}
+            ${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib
+            ${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/bin
     )
 else()
-    find_path(LIBZIP_INCLUDE_DIR_ZIP
-        NAMES zip.h
-        PATH_SUFFIXES libzip
-    )
-    
-    find_path(LIBZIP_INCLUDE_DIR_ZIPCONF
-        NAMES zipconf.h
-    )
-    
     find_library(LIBZIP_LIBRARY
         NAMES zip libzip
+        PATHS
+            ${PC_LIBZIP_LIBRARY_DIRS}
+            ${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib
     )
 endif()
 
-find_package_handle_standard_args(LibZip 
-    REQUIRED_VARS LIBZIP_LIBRARY LIBZIP_INCLUDE_DIR_ZIP LIBZIP_INCLUDE_DIR_ZIPCONF
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LibZip
+    REQUIRED_VARS LIBZIP_LIBRARY LIBZIP_INCLUDE_DIR
+    VERSION_VAR PC_LIBZIP_VERSION
 )
 
-mark_as_advanced(LIBZIP_INCLUDE_DIR_ZIP LIBZIP_INCLUDE_DIR_ZIPCONF LIBZIP_LIBRARY)
+if(LibZip_FOUND AND NOT TARGET LibZip::LibZip)
+    add_library(LibZip::LibZip UNKNOWN IMPORTED)
+    set_target_properties(LibZip::LibZip PROPERTIES
+        IMPORTED_LOCATION "${LIBZIP_LIBRARY}"
+        INTERFACE_INCLUDE_DIRECTORIES "${LIBZIP_INCLUDE_DIR}"
+    )
+    if(PC_LIBZIP_CFLAGS_OTHER)
+        set_target_properties(LibZip::LibZip PROPERTIES
+            INTERFACE_COMPILE_OPTIONS "${PC_LIBZIP_CFLAGS_OTHER}"
+        )
+    endif()
+endif()
+
+mark_as_advanced(LIBZIP_INCLUDE_DIR LIBZIP_LIBRARY)
