63b4a1b5e72d25232987610493ebe023008a56f5
Jordan Hoare <jordanhoare0@gmail.com>
2025-01-11 19:39:07 +0800

    libzip

--- src/framework/cmake/FindLibZip.cmake
+++ src/framework/cmake/FindLibZip.cmake
@@ -1,29 +1,50 @@
-find_package(PkgConfig)
-pkg_check_modules(PC_LIBZIP QUIET libzip)
-
-find_path(LIBZIP_INCLUDE_DIR_ZIP
-    NAMES zip.h
-    HINTS ${PC_LIBZIP_INCLUDE_DIRS})
-
-find_path(LIBZIP_INCLUDE_DIR_ZIPCONF
-    NAMES zipconf.h
-    HINTS ${PC_LIBZIP_INCLUDE_DIRS})
-
-find_library(LIBZIP_LIBRARY
-    NAMES zip)
-
+# FindLibZip.cmake
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(
-    LIBZIP DEFAULT_MSG
-    LIBZIP_LIBRARY LIBZIP_INCLUDE_DIR_ZIP LIBZIP_INCLUDE_DIR_ZIPCONF)
 
-set(LIBZIP_VERSION 0)
+if(WIN32)
+    # For Windows with vcpkg
+    find_path(LIBZIP_INCLUDE_DIR_ZIP zip.h
+        PATHS
+            ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include
+            ${VCPKG_ROOT}/installed/${VCPKG_TARGET_TRIPLET}/include
+        PATH_SUFFIXES
+            libzip
+    )
 
-if (LIBZIP_INCLUDE_DIR_ZIPCONF)
-  FILE(READ "${LIBZIP_INCLUDE_DIR_ZIPCONF}/zipconf.h" _LIBZIP_VERSION_CONTENTS)
-  if (_LIBZIP_VERSION_CONTENTS)
-    STRING(REGEX REPLACE ".*#define LIBZIP_VERSION \"([0-9.]+)\".*" "\\1" LIBZIP_VERSION "${_LIBZIP_VERSION_CONTENTS}")
-  endif ()
-endif ()
+    find_path(LIBZIP_INCLUDE_DIR_ZIPCONF zipconf.h
+        PATHS
+            ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include
+            ${VCPKG_ROOT}/installed/${VCPKG_TARGET_TRIPLET}/include
+        PATH_SUFFIXES
+            libzip
+    )
 
-set(LIBZIP_VERSION ${LIBZIP_VERSION} CACHE STRING "Version number of libzip")
+    find_library(LIBZIP_LIBRARY
+        NAMES zip libzip
+        PATHS
+            ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib
+            ${VCPKG_ROOT}/installed/${VCPKG_TARGET_TRIPLET}/lib
+    )
+else()
+    # For Unix systems
+    find_package(PkgConfig)
+    pkg_check_modules(PC_LIBZIP QUIET libzip)
+    
+    find_path(LIBZIP_INCLUDE_DIR_ZIP zip.h
+        HINTS ${PC_LIBZIP_INCLUDE_DIRS}
+        PATH_SUFFIXES libzip)
+
+    find_path(LIBZIP_INCLUDE_DIR_ZIPCONF zipconf.h
+        HINTS ${PC_LIBZIP_INCLUDE_DIRS})
+
+    find_library(LIBZIP_LIBRARY NAMES zip libzip
+        HINTS ${PC_LIBZIP_LIBRARY_DIRS})
+endif()
+
+find_package_handle_standard_args(LibZip
+    REQUIRED_VARS 
+        LIBZIP_LIBRARY 
+        LIBZIP_INCLUDE_DIR_ZIP 
+        LIBZIP_INCLUDE_DIR_ZIPCONF)
+
+mark_as_advanced(LIBZIP_INCLUDE_DIR_ZIP LIBZIP_INCLUDE_DIR_ZIPCONF LIBZIP_LIBRARY)
